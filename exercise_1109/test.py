55575b95be581b657ca57b708d7485d84898b358
yccccc77 <112915012+yccccc77@users.noreply.github.com>
2023-11-09 11:57:58 +0800

    create test.py

--- exercise_1109/test.py
+++ exercise_1109/test.py
@@ -1,3 +1,54 @@
+# demo is the function name
+def demo(name, age):
+    # print value
+    print(name, age)
+
+# call function
+demo("Ben", 25)
+
+def func1(*args):
+    for i in args:
+        print(i)
+
+func1(20, 40, 60)
+func1(80, 100)
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    # return multiple values separated by comma
+    return addition, subtraction
+
+# get result in tuple format
+res = calculation(40, 10)
+print(res)
+
+
+# function with default argument
+def show_employee(name, salary=9000):
+    print("Name:", name, "salary:", salary)
+
+show_employee("Ben", 12000)
+show_employee("Jessa")
+
+
+# outer function
+def outer_fun(a, b):
+    square = a ** 2
+
+    # inner function
+    def addition(a, b):
+        return a + b
+
+    # call inner function from outer function
+    add = addition(a, b)
+    # add 5 to the result
+    return add + 5
+
+result = outer_fun(5, 10)
+print(result)
+
+
 def addition(num):
     if num:
         # call same function by reducing number by 1
@@ -6,7 +57,4 @@ def addition(num):
         return 0
 
 res = addition(10)
-print(res)
-
-
-
+print("0 到 10 之間的數字總和",res)
