4c96cc204f31420ee4ee3a47d3323e025c251b06
yccccc77 <112915012+yccccc77@users.noreply.github.com>
2023-12-21 11:56:33 +0800

    1

--- stust_python_2023_12_14/python_exercise.py
+++ stust_python_2023_12_14/python_exercise.py
@@ -12,10 +12,10 @@ class Student:
         self.courses = {}
         self.connection = connection
  
-    def add_course(self, course_code, course_name, semester):
-        if semester not in self.courses:
-            self.courses[semester] = []
-        self.courses[semester].append({"code": course_code, "name": course_name})
+    def student_exists(self, student_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT COUNT(*) FROM students WHERE student_id=?", (student_id,))
+        return cursor.fetchone()[0] > 0
  
     def query_course_info(self, semester):
         if semester in self.courses:
@@ -23,6 +23,30 @@ class Student:
             return f"Courses taken by {self.name} in {semester}:", courses_in_semester
         else:
             return f"No courses found for {self.name} in semester {semester}.", None
+ 
+    def add_course(self, course_code, course_name, semester):
+        if semester not in self.courses:
+            self.courses[semester] = []
+ 
+        # Check if the course already exists for the specified semester
+        if any(course["code"] == course_code for course in self.courses[semester]):
+            return "Course already added for the specified semester."
+ 
+        self.courses[semester].append({"code": course_code, "name": course_name})
+ 
+        # Save student information to the database using INSERT OR REPLACE
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT OR REPLACE INTO students (student_id, name) VALUES (?, ?)",
+                       (self.student_id, self.name))
+ 
+        # Save the course information to the database
+        for course in self.courses[semester]:
+            cursor.execute("INSERT OR REPLACE INTO courses (student_id, semester, course_code, course_name) VALUES (?, ?, ?, ?)",
+                           (self.student_id, semester, course["code"], course["name"]))
+ 
+        self.connection.commit()
+ 
+        return "Course added successfully."
  
     def get_all_courses_info(self):
         all_courses_info = []
@@ -36,6 +60,20 @@ class Student:
                     "course_name": course["name"]
                 })
         return all_courses_info
+ 
+ 
+    def save_courses_to_database(self, semester):
+        cursor = self.connection.cursor()
+ 
+        # Delete existing course information for the specified semester
+        cursor.execute("DELETE FROM courses WHERE student_id=? AND semester=?", (self.student_id, semester))
+ 
+        # Insert or replace the updated course information for the specified semester
+        for course in self.courses[semester]:
+            cursor.execute("INSERT OR REPLACE INTO courses (student_id, semester, course_code, course_name) VALUES (?, ?, ?, ?)",
+                           (self.student_id, semester, course["code"], course["name"]))
+ 
+        self.connection.commit()
  
     def save_to_database(self):
         cursor = self.connection.cursor()
@@ -98,7 +136,7 @@ def generate_sample_data(connection):
             semester = "Fall 2023"
             student.add_course(course_code, course_name, semester)
  
-        student.save_to_database()
+        #student.save_to_database()
         sample_students.append(student)
  
     return sample_students
@@ -108,7 +146,15 @@ class StudentGUI(QWidget):
         super().__init__()
  
         self.connection = sqlite3.connect('students.db')
-        create_tables(self.connection)
+ 
+        # Check if the students table exists in the database
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='students'")
+        table_exists = cursor.fetchone() is not None
+ 
+        if not table_exists:
+            create_tables(self.connection)
+            self.generate_sample_data()
  
         self.init_ui()
  
@@ -130,6 +176,12 @@ class StudentGUI(QWidget):
         self.setGeometry(300, 300, 500, 400)
         self.setWindowTitle('STUST Student Information and Course Query Tool')
  
+     
+    def student_exists(self, student_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT COUNT(*) FROM students WHERE student_id=?", (student_id,))
+        return cursor.fetchone()[0] > 0
+     
     def create_add_course_tab(self):
         tab = QWidget()
         layout = QVBoxLayout()
@@ -160,6 +212,74 @@ class StudentGUI(QWidget):
         tab.setLayout(layout)
         return tab
  
+    def create_add_course_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout()
+ 
+        self.student_id_label_add = QLabel('Student ID:')
+        self.student_id_edit_add = QLineEdit()
+        self.course_code_label_add = QLabel('Course Code:')
+        self.course_code_edit_add = QLineEdit()
+        self.course_name_label_add = QLabel('Course Name:')
+        self.course_name_edit_add = QLineEdit()
+        self.semester_label_add = QLabel('Semester:')
+        self.semester_edit_add = QLineEdit()
+ 
+        self.add_course_button_add = QPushButton('Add Course')
+ 
+        self.add_course_button_add.clicked.connect(self.add_course)
+ 
+        layout.addWidget(self.student_id_label_add)
+        layout.addWidget(self.student_id_edit_add)
+        layout.addWidget(self.course_code_label_add)
+        layout.addWidget(self.course_code_edit_add)
+        layout.addWidget(self.course_name_label_add)
+        layout.addWidget(self.course_name_edit_add)
+        layout.addWidget(self.semester_label_add)
+        layout.addWidget(self.semester_edit_add)
+        layout.addWidget(self.add_course_button_add)
+ 
+        tab.setLayout(layout)
+        return tab
+ 
+    def add_course(self):
+        student_id = self.student_id_edit_add.text()
+        course_code = self.course_code_edit_add.text()
+        course_name = self.course_name_edit_add.text()
+        semester = self.semester_edit_add.text()
+ 
+        # Check for missing input data
+        if not student_id or not course_code or not course_name or not semester:
+            self.show_alert("Missing Data", "Please fill in all fields.")
+            return
+ 
+        # Check if the student ID exists in the database
+        if not self.student_exists(student_id):
+            self.show_alert("Invalid Student ID", "Student ID does not exist.")
+            return
+ 
+        # Create or load student profile
+        student = Student(student_id, "", self.connection)
+        student.load_from_database()
+ 
+        # Check if the course is already added for the specified semester
+        if self.course_exists_for_semester(student, course_code, semester):
+            self.show_alert("Course Already Added", "The course is already added for the specified semester.")
+            return
+ 
+        # Add course to the student's profile
+        student.add_course(course_code, course_name, semester)
+ 
+        # Save student course information to the database
+        student.save_courses_to_database(semester)
+ 
+        # Show success message box
+        self.show_success_message("Course Added", "The course has been successfully added.")
+ 
+    def course_exists_for_semester(self, student, course_code, semester):
+        return semester in student.courses and any(course["code"] == course_code for course in student.courses[semester])
+ 
+ 
     def create_query_course_tab(self):
         tab = QWidget()
         layout = QVBoxLayout()
@@ -183,6 +303,36 @@ class StudentGUI(QWidget):
         tab.setLayout(layout)
         return tab
  
+    def query_courses(self):
+        student_id = self.student_id_edit_query.text()
+        semester = self.semester_edit_query.text()
+ 
+        # Check for missing input data
+        if not student_id or not semester:
+            self.show_alert("Missing Data", "Please fill in all fields.")
+            return
+ 
+        # Check if the student ID exists in the database
+        if not self.student_exists(student_id):
+            self.show_alert("Invalid Student ID", "Student ID does not exist.")
+            return
+ 
+        # Create or load student profile
+        student = Student(student_id, "", self.connection)
+        student.load_from_database()
+ 
+        # Query course information
+        result_message, courses_info = student.query_course_info(semester)
+ 
+        # Display result in the QTextBrowser
+        self.result_browser_query.setPlainText(result_message)
+        if courses_info:
+            for course in courses_info:
+                self.result_browser_query.append(f"{course['code']}: {course['name']}")
+ 
+ 
+ 
+ 
     def create_display_all_tab(self):
         tab = QWidget()
         layout = QVBoxLayout()
@@ -198,32 +348,7 @@ class StudentGUI(QWidget):
         tab.setLayout(layout)
         return tab
  
-    def add_course(self):
-        student_id = self.student_id_edit_add.text()
-        name = self.name_edit_add.text()
-        course_code = self.course_code_edit_add.text()
-        course_name = self.course_name_edit_add.text()
- 
-        # Check for missing input data
-        if not student_id or not name or not course_code or not course_name:
-            self.show_alert("Missing Data", "Please fill in all fields.")
-            return
- 
-        # Create or load student profile
-        student = Student(student_id, name, self.connection)
-        student.load_from_database()
- 
-        semester = "Fall 2023"  # Set the default semester to Fall 2023 for simplicity
- 
-        # Add course to the student's profile
-        student.add_course(course_code, course_name, semester)
- 
-        # Save student course information to the database
-        student.save_to_database()
- 
-        # Show success message box
-        self.show_success_message("Course Added", "The course has been successfully added.")
- 
+     
     def query_courses(self):
         student_id = self.student_id_edit_query.text()
         semester = self.semester_edit_query.text()
